495a9aee88b0aac95b6fbddd8d8b91496c0ce358
Andrew Weber <aweber32@jh.edu>
2025-10-21 19:49:25 -0500

    Added potential future states

--- ClueLess/States.py
+++ ClueLess/States.py
@@ -4,3 +4,18 @@ class State:
     SERVER_MENU = 1
     CLIENT_MENU = 2
     GAME = 3
+
+# # --- App States --- #
+# class AppState:
+#     MENU = 0
+#     GAME = 1
+#
+# # --- Menu States --- #
+# class MenuState:
+#     MAIN_MENU = 0
+#     SERVER_MENU = 1
+#     CLIENT_MENU = 2
+#
+# # --- Game States --- #
+# class GameState:
+#     GAME_MENU = 0
