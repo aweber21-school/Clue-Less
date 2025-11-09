79da10d5f229c28c820bd5c7bb8240a3b5e828d0
Adit Rao <aditrao@Adits-MacBook-Pro.local>
2025-11-09 08:56:19 -0800

    Updated distributeCards() and added clarifying comments in Game.py and Constants.py

--- ClueLess/Game.py
+++ ClueLess/Game.py
@@ -53,11 +53,13 @@ class Game:
         ]
         self.updateTilemap()
 
-        # Solution
+        # Initialize self.solution
         self.solution = None
+        
+        # Randomly choose "truth" cards
         self.pickSolution()
 
-        # Distribute cards to all players
+        # Distribute cards to players (without truth set)
         self.distributeCards()
 
         # Log
@@ -132,8 +134,11 @@ class Game:
         )
 
     def distributeCards(self):
-        """Distributes cards to all of the players"""
+        """Removes truth set, then distributes remaining cards to players"""
         allCards = list(Cards.CHARACTERS + Cards.WEAPONS + Cards.ROOMS)
+        for item in self.solution:
+                if item in allCards:
+                    allCards.remove(item)
 
         playerIndex = 0
         while len(allCards) > 0:
